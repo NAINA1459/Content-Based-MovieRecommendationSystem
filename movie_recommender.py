1a4def886a8cbebc16611ae24f28a5136280b1a0
NAINA MAJUMDAR <79380764+NAINA1459@users.noreply.github.com>
2021-07-20 15:30:11 +0530

    Update movie_recommender.py

--- movie_recommender.py
+++ movie_recommender.py
@@ -10,12 +10,11 @@ def get_index_from_title(title):
 	return df[df.title == title]["index"].values[0]
 ##################################################
 
-##Read CSV File
 df = pd.read_csv("movie_dataset.csv")
 print (df.columns)
 ##Select Features
 features = ['keywords','cast','genres']
-##Create a column in DF which combines all selected features
+
 for feature in features:
 	df[feature] = df[feature].fillna('')
 
@@ -27,23 +26,23 @@ def combine_features(row):
 df["combined_features"] = df.apply(combine_features,axis=1)
 
 print (df["combined_features"].head())
-##Create count matrix from this new combined column
+
 cv = CountVectorizer()
 
 count_matrix = cv.fit_transform(df["combined_features"])
-##Compute the Cosine Similarity based on the count_matrix
+
 cosine_sin = cosine_similarity(count_matrix)
 movie_user_likes = "Avatar"
 
-##Get index of this movie from its title
+
 movie_index = get_index_from_title(movie_user_likes)
 
 similar_movies = list(enumerate(cosine_sin[movie_index]))
 
-## Get a list of similar movies in descending order of similarity score
+
 
 sorted_similar_movies = sorted(similar_movies,key= lambda x:x[1], reverse=True)
-##Print titles of first 50 movies
+
 i=0
 for movie in sorted_similar_movies:
 	print (get_title_from_index(movie[0]))
